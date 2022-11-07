c6e3e09942eb9517250d4300dbf2e26ede63f706
Gocra <116901522+Gocra@users.noreply.github.com>
2022-11-07 20:21:09 +0000

    Add files via upload

--- src/Hamburger/Hamburger.js
+++ src/Hamburger/Hamburger.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const HamburgerIconContainer = styled.div`
+const Container = styled.div`
   cursor: pointer;
   width: 30px;
   height: 30px;
@@ -10,24 +10,31 @@ const HamburgerIconContainer = styled.div`
   flex-direction: column;
 `;
 
-const HamburgerIconLine = styled.div`
+const Icon = styled.div`
+  height: 22px;
+  display: flex;
+  width: 22px;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const Line = styled.div`
   width: 22px;
   height: 2px;
   background-color: #fff;
-  margin-top: 4px;
-  margin-bottom: 4px;
+  margin-top: 2px;
   border-radius: 6px;
 `;
 
 const Hamburger = () => {
   return (
-    <HamburgerIconContainer>
-      <div>
-        <HamburgerIconLine />
-        <HamburgerIconLine />
-        <HamburgerIconLine />
-      </div>
-    </HamburgerIconContainer>
+    <Container>
+      <Icon>
+        <Line />
+        <Line />
+        <Line />
+      </Icon>
+    </Container>
   );
 };
 
